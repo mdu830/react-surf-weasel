6b1b36db0af9b2e63c2c79b9cd3a1d07b7b7beca
mdu830 <mdu830@yahoo.com>
2021-06-22 16:00:13 -0400

    recreated react app and created very basic working server

---
+++ server.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV !== 'production') {
+    console.log('loading dev environments');
+    // require('dotenv').config();
+}
+// require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
