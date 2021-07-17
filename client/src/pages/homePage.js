dd35bed12dad7a5b1186c30fd2fcd555beec28f7
mdu830 <mdu830@yahoo.com>
2021-07-17 00:59:05 -0400

    installed @materials-ui and created better carousel

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,22 +1,12 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
 import React, { useState } from 'react';
-import {
-    Carousel,
-    CarouselItem,
-    CarouselControl,
-    CarouselIndicators,
-    CarouselCaption
-} from 'reactstrap';
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@material-ui/core'
 
 import waveOrange from '../assets/images/waveOrange.jpg'
 import waveBlue from '../assets/images/waveBlue.jpg'
 import waveGreen from '../assets/images/waveGreen.jpg'
-
-// import { fadeIn } from 'react-animations'
-// import styled, {keyframes} from 'styled-components'
-
-// const Animate = styled.div`animation: 1.3s ${keyframes`${fadeIn}`} `;
 
 const items = [
     {
@@ -37,51 +27,11 @@ const items = [
 ];
 
 const homePage = (props) => {
-    
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
 
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
-    }
-
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    }
-
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    }
-
-    const slides = items.map((item) => {
-        return (
-            <CarouselItem
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(true)}
-                key={item.src}
-            >
-                <img className="slideImg" src={item.src} alt={item.altText} />
-                <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
-            </CarouselItem>
-        );
-    });
 
     return (
-        <Carousel
-            fade={true}
-            activeIndex={activeIndex}
-            next={next}
-            previous={previous}
-        >
-            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-            {slides}
-            <CarouselControl direction="prev" directionText=" " onClickHandler={previous} />
-            <CarouselControl direction="next" directionText=" " onClickHandler={next} />
+        <Carousel>
+            {items.map((item) => <img className="slideImg" src={item.src} alt={item.altText} />)}
         </Carousel>
     );
 }
