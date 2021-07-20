a4855539627ebd950c715c87f4349efc2159e1dc
mdu830 <mdu830@yahoo.com>
2021-07-20 04:15:32 -0400

    routes test 3

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
+      })
 }
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);
