1dca48f2dd86478360368556a73fb310fb23ee6a
mdu830 <mdu830@yahoo.com>
2021-07-19 22:32:38 -0400

    added form input and more styling to searchBar

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -2,60 +2,71 @@ import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import SearchIcon from '@material-ui/icons/Search'; import {
+import SearchIcon from '@material-ui/icons/Search';
+import {
     Navbar,
-    Button
+    Button,
+    Input
 } from 'reactstrap';
 
-const useStyles = makeStyles((theme) => ({
-    margin: {
-        margin: theme.spacing(1),
-    },
-    marginLeft: {
-        marginLeft: theme.spacing(3),
-    },
-    marginRight: {
-        marginRight: theme.spacing(4),
-    },
-    marginBottom: {
-        marginBottom: theme.spacing(.5),
-    },
-}));
+// const useStyles = makeStyles((theme) => ({
+//     margin: {
+//         margin: theme.spacing(1),
+//     },
+//     marginLeft: {
+//         marginLeft: theme.spacing(3),
+//     },
+//     marginRight: {
+//         marginRight: theme.spacing(4),
+//     },
+//     marginBottom: {
+//         marginBottom: theme.spacing(.5),
+//     },
+// }));
 
-const SearchBar = () => {
+class SearchBar extends React.Component {
 
-    const classes = useStyles();
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        console.log('A beach was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
 
 
-    return (
-        <Navbar className="searchBar" color="light" light expand="md">
-            <div className={classes.margin}>
+    render() {
+        return (
+            <Navbar className="searchBar" color="light" light expand="md">
                 <Grid container spacing={1} alignItems="flex-end">
-                    <Grid item className={classes.marginLeft}>
-                        <SearchIcon />
+
+                    <Grid item>
+                        <form className="searchForm" onSubmit={this.handleSubmit}>
+                            <SearchIcon className="searchIcon" />
+                            <TextField id="standard-basic" type="text" label="Beach Search" value={this.state.value} onChange={this.handleChange} />
+                        </form>
                     </Grid>
-                    <Grid item className={classes.marginRight}>
-                        <TextField id="input-with-icon-grid" label="Beach Search" />
-                    </Grid>
-                    <Grid item className={classes.marginLeft}>
+                    <Grid item>
                         <Button className="beachButtons" outline color="success">Atlantic Beach NC</Button>{' '}
-                    </Grid>
-                    <Grid item className={classes.marginLeft}>
                         <Button className="beachButtons" outline color="success">Indian Beach NC</Button>{' '}
-                    </Grid>
-                    <Grid item className={classes.marginLeft}>
                         <Button className="beachButtons" outline color="success">Emerald Isle NC</Button>{' '}
-                    </Grid>
-                    <Grid item className={classes.marginLeft}>
                         <Button className="beachButtons" outline color="success">Topsail NC</Button>{' '}
                     </Grid>
                 </Grid>
-            </div>
-            <div>
 
-            </div>
-        </Navbar>
-    )
+
+            </Navbar>
+        )
+    }
 }
 
 export default SearchBar;
