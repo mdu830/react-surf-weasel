55b1b7dd111cf2f79a36ee50b41b4f2cc5bc3f7e
mdu830 <mdu830@yahoo.com>
2021-07-20 14:25:46 -0400

    routes test 10 (HashRouter test)

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Router>
+      <Router basename="#/">
           <Switch>
             <Route exact path={["/"]} component={homePage} />
             <Route exact path={["/register"]} component={registerPage} />
