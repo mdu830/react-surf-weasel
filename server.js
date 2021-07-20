25339d44e23bdbaaa2ceab6d61cffc7e48dacda9
mdu830 <mdu830@yahoo.com>
2021-07-20 02:26:20 -0400

    created basic express route to react app in server.js (heroku routes issue)

--- server.js
+++ server.js
@@ -10,10 +10,22 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-  }
+}
+
+  // Error Handler
+app.use( (err, req, res, next) => {
+    console.log('===== ERROR =====');
+    console.error(err.stack);
+    res.status(500);
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
+  })
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);
