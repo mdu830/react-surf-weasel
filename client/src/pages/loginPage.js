27389a70f5ac99b7a8c2631b2d68fc20e6ee226b
mdu830 <mdu830@yahoo.com>
2021-07-20 22:33:50 -0400

    got routes working, added header to login and register page

--- client/src/pages/loginPage.js
+++ client/src/pages/loginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../components/header';
 
 const LoginPage = () => {
 
     return(
         <>
         <div className="page">
+            <Header />
             <h1>Login</h1>
         </div>
         </>
--- client/src/pages/registerPage.js
+++ client/src/pages/registerPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../components/header';
 
 const RegisterPage = () => {
 
     return(
         <>
         <div className="page">
+            <Header />
             <h1>Register</h1>
         </div>
         </>
