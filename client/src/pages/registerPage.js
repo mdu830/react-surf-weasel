0ac481f92e09050b2762c640455fd06b5b06b754
mdu830 <mdu830@yahoo.com>
2021-07-21 16:37:32 -0400

    started registerPage styling and conatainer

--- client/src/pages/registerPage.js
+++ client/src/pages/registerPage.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Header from '../components/header';
-import Footer from '../components/footer'
+import Footer from '../components/footer';
+import { fadeInDown } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
+
+const Animate = styled.div`animation: 1.3s ${keyframes`${fadeInDown}`} `;
+
 
 const RegisterPage = () => {
 
-    return(
+    return (
         <>
-        <div className="page">
-            <Header />
-            <h1>Register</h1>
-            <Footer />
-        </div>
+            <div className="page">
+                <Header />
+                <Animate>
+                    <div className="registerCont m-3">
+                        <h1>Register</h1>
+
+                    </div>
+                </Animate>
+                <Footer />
+            </div>
         </>
     )
 }
