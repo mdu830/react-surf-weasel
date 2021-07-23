7aef2bf6e63c44c8bd1e862d1323aaec7bd8a772
mdu830 <mdu830@yahoo.com>
2021-07-23 12:50:02 -0400

    added mongoose user and index models

---
+++ models/users.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// const bcrypt = require('bcryptjs');
+
+const UserSchema = new Schema({
+    firstName: { type: String, trim: true, unique: false },
+    lastName: { type: String, trim: true, unique: false },
+    username: { type: String, trim: true, unique: true, require: true },
+    email: { type: String, trim: true, unique: true, require: true },
+    password: { type: String, trim: true, unique: false, require: true },
+});
+
+UserSchema.methods = {
+    checkPassword: function(inputPassword) {
+        console.log('inputPassword', inputPassword);
+        console.log(this);
+        return bcrypt.compareSync(inputPassword, this.password);
+    },
+    hashPassword: function(plainTextPassword) {
+        return bcrypt.hashSync(plainTextPassword, 10);
+    }
+};
+
+UserSchema.pre('save', function(next) {
+    if (!this.password) next();
+    else {
+        this.password = this.hashPassword(this.password);
+        next();
+    }
+})
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
