fbed05bfc47efca8c4aa6fc80b465b2f4497ecec
mdu830 <mdu830@yahoo.com>
2021-07-24 13:50:47 -0400

    cleaned up old code

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Carousel from '../components/carousel'
 import SearchBar from '../components/searchBar';
 import Footer from '../components/footer';
-// import Header from '../components/header';
 
 
 const Homepage = () => {
@@ -10,7 +9,6 @@ const Homepage = () => {
   return (
 
     <div>
-      {/* <Header /> */}
       <Carousel />
       <SearchBar />
       <Footer />
--- client/src/pages/registerPage.js
+++ client/src/pages/registerPage.js
@@ -1,44 +1,35 @@
 import React from 'react';
-// import Header from '../components/header';
 import Footer from '../components/footer';
-// import { fadeInUp } from 'react-animations';
-// import styled, { keyframes } from 'styled-components';
 import { Grid, TextField } from '@material-ui/core';
 import { Button } from 'reactstrap';
-
-
-// const Animate = styled.div`animation: 1.3s ${keyframes`${fadeInUp}`} `;
 
 const RegisterPage = () => {
 
     return (
         <>
             <div>
-                {/* <Header /> */}
-                {/* <Animate> */}
-                    <div className="registerCont">
-                        <div className="m-4">
-                            <h1>Register</h1>
-                            <div className="">
-                                <Grid >
-                                    <Grid item xs={12}>
-                                        <TextField required className="m-2" id="standard-required" label="Email Address" defaultValue="" />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField required className="m-2" id="standard-disabled" type="password" label="Password" defaultValue="" />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField required className="m-2" id="standard-disabled" type="password" label="Confirm Password" defaultValue="" />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField className="m-2" id="standard-disabled" label="Favorite Beach" defaultValue="" />
-                                    </Grid>
+                <div className="registerCont">
+                    <div className="m-4">
+                        <h1>Register</h1>
+                        <div className="">
+                            <Grid >
+                                <Grid item xs={12}>
+                                    <TextField required className="m-2" id="standard-required" label="Email Address" defaultValue="" />
                                 </Grid>
-                                <Button className="m-2 signupButton" color="outline-success">Sign Up</Button>
-                            </div>
+                                <Grid item xs={12}>
+                                    <TextField required className="m-2" id="standard-disabled" type="password" label="Password" defaultValue="" />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField required className="m-2" id="standard-disabled" type="password" label="Confirm Password" defaultValue="" />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField className="m-2" id="standard-disabled" label="Favorite Beach" defaultValue="" />
+                                </Grid>
+                            </Grid>
+                            <Button className="m-2 signupButton" color="outline-success">Sign Up</Button>
                         </div>
                     </div>
-                {/* </Animate> */}
+                </div>
                 <Footer />
             </div>
         </>
