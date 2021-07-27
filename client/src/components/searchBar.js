132f4f1288bd462a7d00c213c8edbc255149e879
mdu830 <mdu830@yahoo.com>
2021-07-26 21:25:37 -0400

    got searchValue and buttonValues to pass to surfRportPage, still need to query an api

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -1,48 +1,77 @@
-import React, { useState } from 'react';
-import { TextField } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { setRef, TextField } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
 import { Navbar, Button } from 'reactstrap';
+import { useHistory } from "react-router-dom";
 
-class SearchBar extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { value: '' };
+const SearchBar = (props) => {
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { value: '' };
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    // }
 
-    handleSubmit(event) {
-        console.log('A beach was submitted: ' + this.state.value);
-        event.preventDefault();
+    let history = useHistory();
+
+    const [searchValue, setSearchValue] = useState("");
+    // const [buttonValue, setButtonValue] = useState("");
+
+    
+    const handleSubmit = (evt) => {
         
+        if(searchValue !== "") {
+
+            console.log('A beach was submitted: ' + searchValue);
+
+            evt.preventDefault(history.push({
+                pathname: '/report', 
+                state: {
+                    value: searchValue,
+                    
+                }
+            }));
+
+        } else {
+            evt.preventDefault();
+            alert("That ain't a beach");
+
+        }
+
     }
 
-
-    render() {
-        return (
-            <Navbar className="searchBar" color="light" light expand="md">
-                <Grid item>
-                    <form className="searchForm" onSubmit={this.handleSubmit}>
-                        <SearchIcon className="searchIcon" />
-                        <TextField id="standard-basic" type="text" label="Beach Search" value={this.state.value} onChange={this.handleChange} />
-                    </form>
-                </Grid>
-                <Grid item>
-                    <Button className="beachButtons" outline color="success">Atlantic Beach NC</Button>{' '}
-                    <Button className="beachButtons" outline color="success">Indian Beach NC</Button>{' '}
-                    <Button className="beachButtons" outline color="success">Emerald Isle NC</Button>{' '}
-                    <Button className="beachButtons" outline color="success">Topsail NC</Button>{' '}
-                </Grid>
-            </Navbar>
-        )
+    const handleButtonSubmit = (e) => {
+        // setButtonValue(e.target.value);
+        e.preventDefault(history.push({
+            pathname: '/report', 
+            state: {
+                value: e.target.value,
+                
+            }
+        }));
+         
     }
+
+    return (
+        <Navbar className="searchBar" color="light" light expand="md">
+            <Grid item>
+                <form className="searchForm" onSubmit={handleSubmit}>
+                    <SearchIcon className="searchIcon" />
+                    <TextField id="standard-basic" type="text" label="Beach Search" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                </form>
+            </Grid>
+            <Grid item>
+                <Button className="beachButtons" value="Atlantic Beach NC" onClick={(e) => {handleButtonSubmit(e)}} outline color="success">Atlantic Beach NC</Button>{' '}
+                <Button className="beachButtons" value="Indian Beach NC" onClick={(e) => handleButtonSubmit(e)} outline color="success">Indian Beach NC</Button>{' '}
+                <Button className="beachButtons" value="Emerald Isle NC" onClick={(e) => handleButtonSubmit(e)} outline color="success">Emerald Isle NC</Button>{' '}
+                <Button className="beachButtons" value="Topsail NC" onClick={(e) => handleButtonSubmit(e)} outline color="success">Topsail NC</Button>{' '}
+            </Grid>
+        </Navbar>
+    )
 }
 
 export default SearchBar;
--- client/src/pages/surfReportPage.js
+++ client/src/pages/surfReportPage.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SurfReportPage = () => {
+const SurfReportPage = (props) => {
 
+    useEffect(() => {
+        console.log(props.location.state.value);
+    }, []);
+    
     return (
         <div className="page">
             <div className="registerCont">
-                <h1>Surf Report</h1>
+                <h1>{props.location.state.value}</h1>
             </div>
         </div>
     )
