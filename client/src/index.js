89eb90e9b1655ba18e29ab9cd669ba5a5945ce9c
mdu830 <mdu830@yahoo.com>
2021-07-27 17:09:24 -0400

    cleaned up some old code and uninstalled react-query

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { QueryClientProvider, QueryClient } from 'react-query';
-// import axios from 'axios';
 
 
 
