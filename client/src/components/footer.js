bb87bf161387e9b6b995b8d93b7a0eef5e0d0bea
mdu830 <mdu830@yahoo.com>
2021-08-03 15:03:02 -0400

    added year to footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return(
         <footer className="footer">
-            <div className="footerText">&#169; Surf Weasel created by <a href="https://github.com/mdu830">Mdu830</a></div>
+            <div className="footerText">&#169; Surf Weasel 2021 created by <a href="https://github.com/mdu830">Mdu830</a></div>
         </footer>
     );
 }
