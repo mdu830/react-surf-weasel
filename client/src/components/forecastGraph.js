fb9ef42b356ae011dd04af12186ad49d23caca7a
mdu830 <mdu830@yahoo.com>
2021-08-06 19:46:08 -0400

    indicated that the current graph isnt functional to user

--- client/src/components/forecastGraph.js
+++ client/src/components/forecastGraph.js
@@ -51,14 +51,14 @@ const ForcastGraph = (data) => {
         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
         datasets: [
             {
-                label: "First dataset",
+                label: "This Graph",
                 data: [33, 53, 85, 41, 44, 65],
                 fill: true,
                 backgroundColor: "rgba(75,192,192,0.2)",
                 borderColor: "rgba(75,192,192,1)"
             },
             {
-                label: "Second dataset",
+                label: "Does Nothing",
                 data: [33, 25, 35, 51, 54, 76],
                 fill: false,
                 borderColor: "#742774"
